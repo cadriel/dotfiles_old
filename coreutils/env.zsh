d51db068e7c3bb96a62e6968da34aa8972800523
Craig Bassett <craig@kessel.localdomain>
2017-01-18 15:25:33 -0600

    - Moved the updated ls alias to where it should have been all along.

--- coreutils/env.zsh
+++ coreutils/env.zsh
@@ -7,9 +7,5 @@ export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 # GNU manpages for programs that are GNU ones, and fallback to OSX manpages otherwise
 alias man='_() { echo $1; man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1 1>/dev/null 2>&1;  if [ "$?" -eq 0 ]; then man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1; else man $1; fi }; _'
 
-# Overwrite oh-my-zsh's default ls alias with our own
-unalias ls
-alias ls='ls --color=tty -al'
-
 # Setup solarized dark color support
 eval `dircolors ~/.dir_colors`
