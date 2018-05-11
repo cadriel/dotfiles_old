5f6b6d323268835ef2b544783690e671b2e46880
Craig Bassett <craig.bassett@gmail.com>
2018-05-11 16:43:03 -0500

    updated java default moved npm config to private repo

--- java/env.zsh
+++ java/env.zsh
@@ -2,3 +2,4 @@
 # export JAVA_HOME=`/usr/libexec/java_home`
 # export JAVA_HOME=`/usr/libexec/java_home -v 1.6.0` # Force 1.6 to be our default.
 # export JAVA_HOME=`/usr/libexec/java_home -v 1.7.0` # Force 1.7 to be our default.
+# export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0` # Force 1.8 to be our default.
